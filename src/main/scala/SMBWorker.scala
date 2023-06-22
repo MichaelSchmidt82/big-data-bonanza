488021e1f1917dcfb96d609d235d25f33243293f
Michael Schmidt <michael.schmidt82@gmail.com>
2023-06-21 19:30:32 -0500

    Add SMB worker

---
+++ src/main/scala/SMBWorker.scala
@@ -0,0 +1,35 @@
+import com.hierynomus.msfscc.fileinformation.FileIdBothDirectoryInformation
+import com.hierynomus.smbj.auth.AuthenticationContext
+import com.hierynomus.smbj.SMBClient
+import com.hierynomus.smbj.share.DiskShare
+import scala.collection.convert.ImplicitConversions.`iterable AsScalaIterable`
+
+object SMBWorker {
+
+  private val auth = new AuthenticationContext("foo", "bar".toCharArray, "home.lan")
+  private val client = new SMBClient()
+
+  private val session = client
+    .connect("utility-box.home.lan")
+    .authenticate(auth)
+
+  // Connect to Share
+  private val share: DiskShare = session.connectShare("foo_share").asInstanceOf[DiskShare]
+
+  private val files = share
+    .list("Documents", "*")
+    .filter {
+      _.getFileName matches """^(?!.*\.\_|.DS_Store|^\.{1,2}$).*$"""
+    }
+
+  for (f <- files) {
+    println(f.getFileName)
+  }
+}
+
+
+
+
+
+
+
