fca1e379b511367e644d462c24f94a5484e198f2
Michael Schmidt <michael.schmidt82@gmail.com>
2023-06-26 09:57:58 -0500

    Add/Start Decompressor

--- src/main/scala/ArchiveUtils.scala
+++ src/main/scala/ArchiveUtils.scala
@@ -1,5 +1,7 @@
 import org.apache.commons.compress.compressors.CompressorStreamFactory
-import java.io.{BufferedOutputStream, FileOutputStream}
+import org.apache.commons.compress.compressors.bzip2.BZip2CompressorInputStream
+
+import java.io.{BufferedInputStream, BufferedOutputStream, File, FileInputStream, FileOutputStream, InputStream}
 
 class Compressor(val OutPath: String, val Filename: String, val Data: Array[Byte]) {
 
@@ -15,4 +17,14 @@ class Compressor(val OutPath: String, val Filename: String, val Data: Array[Byte
     BufferedStream.close()
     Bzip2Compressor.close()
   }
+}
+
+object Decompressor extends App {
+
+  val fi: File = new File("some_file.bz2")
+  val inStream: InputStream = new FileInputStream(fi)
+  val buffed: BufferedInputStream = new BufferedInputStream(inStream)
+  val input: Array[Byte] = new BZip2CompressorInputStream(buffed).readAllBytes()
+
+  println(input)
 }
