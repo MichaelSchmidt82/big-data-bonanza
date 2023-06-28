dda58f589552831f2b5d0e13f388e534c10de8de
Michael Schmidt <michael.schmidt@vumc.org>
2023-06-28 16:28:03 -0500

    Cleanup file

--- src/main/scala/Archive.scala
+++ src/main/scala/Archive.scala
@@ -1,40 +1,37 @@
+import org.apache.commons.compress.compressors.bzip2.BZip2CompressorOutputStream
+import java.io.{File, FileInputStream, FileOutputStream}
+
 trait Archive {
 
 
-  import org.apache.commons.compress.compressors.bzip2.{BZip2CompressorInputStream, BZip2CompressorOutputStream}
-  import java.io.{BufferedInputStream, File, FileInputStream, FileOutputStream, InputStream}
-
-  trait Archive {
-
     // Default to BZip2 Format
-    def compressToBzip2(files: Seq[File], outputPath: String): Unit = {
+  def compressToBzip2(files: Seq[File], outputPath: String): Unit = {
 
-      val outputFile = new File(outputPath)
-      val outputStream = new FileOutputStream(outputFile)
-      val bzip2OutputStream = new BZip2CompressorOutputStream(outputStream)
+    val outputFile = new File(outputPath)
+    val outputStream = new FileOutputStream(outputFile)
+    val bzip2OutputStream = new BZip2CompressorOutputStream(outputStream)
 
-      try {
-        for (file <- files) {
-          val inputStream = new FileInputStream(file)
-          val buffer = new Array[Byte](4096)
-          var bytesRead = inputStream.read(buffer)
+    try {
+      for (file <- files) {
+        val inputStream = new FileInputStream(file)
+        val buffer = new Array[Byte](4096)
+        var bytesRead = inputStream.read(buffer)
 
-          while (bytesRead != -1) {
-            bzip2OutputStream.write(buffer, 0, bytesRead)
-            bytesRead = inputStream.read(buffer)
-          }
-
-          inputStream.close()
+        while (bytesRead != -1) {
+          bzip2OutputStream.write(buffer, 0, bytesRead)
+          bytesRead = inputStream.read(buffer)
         }
-      } finally {
-        bzip2OutputStream.finish()
-        bzip2OutputStream.close()
-        outputStream.close()
+
+        inputStream.close()
       }
+    } finally {
+      bzip2OutputStream.finish()
+      bzip2OutputStream.close()
+      outputStream.close()
     }
-
-    def decompress(): Unit
-
-    def integrationTest(): Unit = {}
   }
+
+  def decompress(): Unit
+
+  def integrationTest(): Unit = {}
 }
