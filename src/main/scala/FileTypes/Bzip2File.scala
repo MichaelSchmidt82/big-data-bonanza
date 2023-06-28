8e60ce2e6bdb2ade4698e8d7f2a14e02b8116bba
Michael Schmidt <michael.schmidt@vumc.org>
2023-06-28 17:39:43 -0500

    overload all compressions to return a single file, but accept multiple file types

--- src/main/scala/FileTypes/Bzip2File.scala
+++ src/main/scala/FileTypes/Bzip2File.scala
@@ -1,12 +1,12 @@
 package FileTypes
 
-import org.apache.commons.compress.compressors.bzip2.BZip2CompressorInputStream
+import org.apache.commons.compress.compressors.bzip2.{BZip2CompressorInputStream, BZip2CompressorOutputStream}
 
 import java.io._
 
-class Bzip2File(path: String) extends Archive {
+class Bzip2File(val path: String) extends Archive {
 
-  def decompress(): Unit = {
+  def decompress(): Seq[File] = {
 
     // Input Streams
     val file: File = new File(path)
@@ -29,7 +29,45 @@ class Bzip2File(path: String) extends Archive {
 
       entryName += 1
     }
-
     bzip2InputStream.close()
+  }
+}
+
+object Bzip2File {
+
+  // Default to BZip2 Format
+  def compress(files: Seq[File], outputPath: String): File = {
+
+    val outputFile = new File(outputPath)
+    val outputStream = new FileOutputStream(outputFile)
+    val bzip2OutputStream = new BZip2CompressorOutputStream(outputStream)
+
+    try {
+      for (file <- files) {
+        val inputStream = new FileInputStream(file)
+        val buffer = new Array[Byte](4096)
+        var bytesRead = inputStream.read(buffer)
+
+        while (bytesRead != -1) {
+          bzip2OutputStream.write(buffer, 0, bytesRead)
+          bytesRead = inputStream.read(buffer)
+        }
+
+        inputStream.close()
+      }
+    } finally {
+      bzip2OutputStream.finish()
+      bzip2OutputStream.close()
+      outputStream.close()
+    }
+  }
+
+  def compress(zipFile: ZipFile): File = {
+    val files: Seq[File] = zipFile.decompress()
+    this.compress(files, zipFile.path)
+  }
+
+  def compress(sevenZip: SevenZip): File = {
+    new File("foo")
   }
 }
