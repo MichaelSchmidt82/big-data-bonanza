13d82a3a6fcd3e959461f906347f258655861c20
Michael Schmidt <michael.schmidt@vumc.org>
2023-06-28 17:38:40 -0500

    Update Archive to require decompress only

--- src/main/scala/FileTypes/Archive.scala
+++ src/main/scala/FileTypes/Archive.scala
@@ -1,40 +1,10 @@
 package FileTypes
 
 import org.apache.commons.compress.compressors.bzip2.BZip2CompressorOutputStream
-
 import java.io.{File, FileInputStream, FileOutputStream}
 
-trait Archive {
 
-
-    // Default to BZip2 Format
-  def compressToBzip2(files: Seq[File], outputPath: String): Unit = {
-
-    val outputFile = new File(outputPath)
-    val outputStream = new FileOutputStream(outputFile)
-    val bzip2OutputStream = new BZip2CompressorOutputStream(outputStream)
-
-    try {
-      for (file <- files) {
-        val inputStream = new FileInputStream(file)
-        val buffer = new Array[Byte](4096)
-        var bytesRead = inputStream.read(buffer)
-
-        while (bytesRead != -1) {
-          bzip2OutputStream.write(buffer, 0, bytesRead)
-          bytesRead = inputStream.read(buffer)
-        }
-
-        inputStream.close()
-      }
-    } finally {
-      bzip2OutputStream.finish()
-      bzip2OutputStream.close()
-      outputStream.close()
-    }
-  }
-
-  def decompress(): Unit
+trait Archive { def decompress(): Seq[File]
 
   def integrationTest(): Unit = {}
 }
