5115ee9f216b26a04da11ec0381a97b5c9c9f67a
Michael Schmidt <michael.w.schmidt@proton.me>
2023-07-16 18:04:30 -0700

    Saving WIP

--- src/main/scala/Filetypes/SevenZip.scala
+++ src/main/scala/Filetypes/SevenZip.scala
@@ -1,7 +1,7 @@
 package Filetypes
 
 import io.netty.buffer.ByteBuf
-import org.apache.commons.compress.archivers.sevenz.SevenZFile
+import org.apache.commons.compress.archivers.sevenz.{SevenZArchiveEntry, SevenZFile}
 import org.apache.commons.compress.archivers.zip.ZipArchiveInputStream
 import org.apache.commons.compress.utils.SeekableInMemoryByteChannel
 
@@ -12,29 +12,37 @@ import scala.collection.mutable.ListBuffer
 class SevenZip(val path: String) extends App {
 
   def decompress(): List[File] = {
-
-    val sevenZFile = new File(path)
+    val sevenZFile = new SevenZFile(new File(path))
     val decompressedFiles = ListBuffer.empty[File]
 
-    val sevenFileInputStream = new FileInputStream(sevenZFile)
-
     @tailrec
-    def extractEntry() = {
-      val entry = sevenZFile.getNextEntry
-      val content = new Array[Byte](entry.getSize)
+    def extractEntry(entry: SevenZArchiveEntry): Unit = {
+      if (entry != null) {
 
-      val outputFile = new FileOutputStream(outputFilePath)
+        val outputFilePath = new File(entry.getName)
+        val outputFile = new FileOutputStream(outputFilePath)
 
-      @tailrec
-      def writeBytes(content: Array[Byte], bytesRead: Int): Unit = {
-        if (bytesRead != -1) {
-          outputFile.write(content, 0, bytesRead)
-          writeBytes(content, sevenFileInputStream.read())
+        @tailrec
+        def writeBytes(content: Array[Byte], bytesRead: Int): Unit = {
+          if (bytesRead != -1) {
+            outputFile.write(content, 0, bytesRead)
+            writeBytes(content, sevenZFile.read())
+          }
         }
+
+        val content = new Array[Byte](4096)
+
+        writeBytes(content, sevenZFile.read(content))
+        outputFile.close()
+        decompressedFiles += outputFilePath
       }
+      extractEntry(sevenZFile.getNextEntry)
     }
 
+    extractEntry(sevenZFile.getNextEntry)
 
+    // close files
+
+    decompressedFiles.toList
   }
-
 }
